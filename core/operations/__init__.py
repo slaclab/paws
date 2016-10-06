ad11790f1c8fa993eb717bc16b661f65a81bf261
Lenson Pellouchoud <lensonp@gmail.com>
2016-10-05 18:48:26 -0700

    fixed op import error msg

--- core/operations/__init__.py
+++ core/operations/__init__.py
@@ -11,10 +11,10 @@ mods = [mod for mod in mods if mod[1] not in ['__init__','slacxop','slacxopman',
 op_list = []
 cat_list = []
 for modloader, modname, ispkg in mods:
-    try:
-        mod = importlib.import_module('.'+modname,'core.operations')
-        # iterate through the module's __dict__, find Operations 
-        for name, item in mod.__dict__.items():
+    mod = importlib.import_module('.'+modname,'core.operations')
+    # iterate through the module's __dict__, find Operations 
+    for name, item in mod.__dict__.items():
+        try:
             # is it a class?
             if isinstance(item,type):
                 # is it a non-abstract subclass of Operation?
@@ -25,9 +25,9 @@ for modloader, modname, ispkg in mods:
                     for cat in cats:
                         if not cat in cat_list:
                             cat_list.append(cat)
-    except ImportError:
-        print '[{}] had trouble dealing with module attribute {}: {}'.format(__name__,name,item)
-        raise
+        except ImportError:
+            print '[{}] had trouble dealing with module attribute {}: {}'.format(__name__,name,item)
+            raise
 
  
 
