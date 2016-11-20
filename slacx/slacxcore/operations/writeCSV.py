775432476f7a575cb760cbc5ca90a520d74dcdb0
Amanda <amanda.p.fournier@gmail.com>
2016-11-19 16:32:47 -0800

    The day of never-ending bugs in writeCSV continues, as it will in perpetuity

--- slacx/slacxcore/operations/writeCSV.py
+++ slacx/slacxcore/operations/writeCSV.py
@@ -1,5 +1,5 @@
-from os.path import join, splitext
-from os import listdir, linesep, remove
+from os.path import join, splitext, exists, isfile, split, isdir
+from os import listdir, linesep, remove, getcwd
 import numpy as np
 
 from slacxop import Operation
@@ -43,14 +43,14 @@ class WriteTemperatureIndex(Operation):
         outfile = open(outloc, 'w')
         outfile.write("#TEMPCELSIUS,FILENAME")
         for ii in range(len(csvnames)):
-            csvmsg ='"%s"'% csvnames[ii]
+            #csvmsg ='"%s"'% csvnames[ii]
+            csvmsg = csvnames[ii]
             msg = str(temperatures[ii])+","+csvmsg+linesep
             outfile.write(msg)
         outfile.close()
         self.outputs['filenames'] = csvnames
         self.outputs['temperatures'] = temperatures
         self.outputs['temperature_index_file'] = outloc
-
 
 class ReadTemperatureIndex(Operation):
     """Read temperature index file written by WriteTemperatureIndex."""
@@ -73,13 +73,13 @@ class ReadTemperatureIndex(Operation):
         self.outputs['filenames'] = np.loadtxt(outloc, dtype=str, delimiter=',', skiprows=1, usecols=(1))
         self.outputs['temperature_index_file'] = outloc
 
-class SelectClosestTemperatureBackground(Operation):
+class SelectClosestTemperatureBackgroundFromTemperature(Operation):
     """Read temperature index file written by WriteTemperatureIndex."""
 
     def __init__(self):
         input_names = ['directory','this_temperature']
         output_names = ['background_q','background_I']
-        super(SelectClosestTemperatureBackground, self).__init__(input_names, output_names)
+        super(SelectClosestTemperatureBackgroundFromTemperature, self).__init__(input_names, output_names)
         # docstrings
         self.input_doc['directory'] = "path to directory with background .csv's and .txt headers in it"
         self.input_doc['this_temperature'] = "temperature we want to find a background for"
@@ -88,6 +88,8 @@ class SelectClosestTemperatureBackground(Operation):
         # source & type
         self.input_src['directory'] = optools.fs_input
         self.input_src['this_temperature'] = optools.wf_input
+        self.input_type['directory'] = optools.str_type
+        self.input_type['this_temperature'] = optools.float_type
         self.categories = ['1D DATA PROCESSING.BACKGROUND SUBTRACTION']
 
     def run(self):
@@ -101,6 +103,41 @@ class SelectClosestTemperatureBackground(Operation):
         index_of_best_temp = np.where(diff == diff.min())[0][0]
         file_of_best_temp = filenames[index_of_best_temp]
         q, I = read_csv_q_I(file_of_best_temp)
+        self.outputs['background_q'] = q
+        self.outputs['background_I'] = I
+
+
+class SelectClosestTemperatureBackgroundFromHeader(Operation):
+    """Read temperature index file written by WriteTemperatureIndex."""
+
+    def __init__(self):
+        input_names = ['directory','this_header']
+        output_names = ['background_q','background_I']
+        super(SelectClosestTemperatureBackgroundFromHeader, self).__init__(input_names, output_names)
+        # docstrings
+        self.input_doc['directory'] = "path to directory with background .csv's and .txt headers in it"
+        self.input_doc['this_header'] = "header of this data; will use entry *temp_celsius*"
+        self.output_doc['background_q'] = 'appropriate background q'
+        self.output_doc['background_I'] = 'appropriate background I'
+        # source & type
+        self.input_src['directory'] = optools.fs_input
+        self.input_src['this_header'] = optools.wf_input
+        self.input_type['directory'] = optools.str_type
+        self.categories = ['1D DATA PROCESSING.BACKGROUND SUBTRACTION']
+
+    def run(self):
+        directory = self.inputs['directory']
+        this_temperature = self.inputs['this_header']['temp_celsius']
+        indexname = 'temperature_index.csv'
+        indexloc = join(directory,indexname)
+        temperatures = np.loadtxt(indexloc, dtype=float, delimiter=',', skiprows=1, usecols=(0,))
+        filenames = np.loadtxt(indexloc, dtype=str, delimiter=',', skiprows=1, usecols=(1,))
+        diff = np.fabs(temperatures - this_temperature)
+        index_of_best_temp = np.where(diff == diff.min())[0][0]
+        file_of_best_temp = filenames[index_of_best_temp]
+        print "file_of_best_temp", file_of_best_temp
+        q, I = read_csv_q_I(file_of_best_temp)
+        print "q, I read",
         self.outputs['background_q'] = q
         self.outputs['background_I'] = I
 
@@ -188,6 +225,7 @@ def csvname_from_imagename(imagename):
 
 
 def read_csv_q_I(nameloc):
+    directory = split(nameloc)[0]
     data = np.loadtxt(nameloc, dtype=float, delimiter=',', skiprows=1, usecols=(0,1))
     q = data[:,0]
     I = data[:,1]
@@ -200,3 +238,16 @@ def read_csv_q_I_dI(nameloc):
     dI = data[:,2]
     return q, I, dI
 
+'''
+from os.path import exists, isfile, split, isdir
+nameloc = "/Users/Amanda/Data20161118/ODE/ODE_SAXS1_0002.csv"
+nameloc = "/Users/Amanda/Data20161118/ODE/ODE_SAXS1_0002.csv"
+import numpy as np
+data = np.loadtxt(nameloc, dtype=float, delimiter=',', skiprows=1, usecols=(0,1))
+data1 = np.loadtxt(nameloc, dtype=float, delimiter=',', skiprows=1, usecols=(0,1))
+data2 = np.loadtxt(nameloc, dtype=float, delimiter=',', skiprows=1, usecols=(0,1))
+print "file exists?: ", exists(nameloc)
+
+(data1 == data).all()
+(data1 == data2).all()
+'''
