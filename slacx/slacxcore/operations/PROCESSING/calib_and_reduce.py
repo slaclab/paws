07773d009d6a26d6aa5615928eb37f3a6d211d2f
Amanda <amanda.p.fournier@gmail.com>
2017-01-17 15:51:57 -0800

    Changes to calib_and_reduce: - Corrected assumption that image is square to arbitrary rectangular - Added nominal acceptance of user-defined mask (untested) - Placeholder inputs for gain and readnoise - Placeholder output for error estimate saxs.py:  testing-purposes-only code added, will clean up later.

--- slacx/slacxcore/operations/PROCESSING/calib_and_reduce.py
+++ slacx/slacxcore/operations/PROCESSING/calib_and_reduce.py
@@ -26,14 +26,25 @@ class ReduceByWXDDict(Operation):
         + ' PP (polarization factor), pixel_size, and d_pixel')
         self.input_doc['pixel_size'] = 'pixel size in microns'
         self.input_doc['fpolz'] = 'polarization factor'
-        self.input_src['pixel_size'] = optools.user_input 
+        self.input_doc['gain'] = 'detector gain'
+        self.input_doc['readnoise'] = 'detector readnoise'
+        self.input_doc['mask'] = str('Bad pixel locations, if known (e.g., inactive detector regions, dead pixels,'
+                                     + ' zingers/cosmic rays).  Boolean array of same shape as image or None.')
+        self.input_src['pixel_size'] = optools.user_input
         self.input_src['fpolz'] = optools.user_input
+        self.input_src['gain'] = optools.user_input
+        self.input_src['readnoise'] = optools.user_input
+        self.input_src['mask'] = optools.user_input
         self.input_type['pixel_size'] = optools.float_type
         self.input_type['fpolz'] = optools.float_type
         self.inputs['pixel_size'] = 79 
         self.inputs['fpolz'] = 0.95 
+        self.inputs['gain'] = 1.0
+        self.inputs['readnoise'] = 1.0
+        self.inputs['mask'] = None
         self.output_doc['q'] = 'Scattering vector magnitude q'
         self.output_doc['I_of_q'] = 'Integrated intensity at q'
+        self.output_doc['dI_of_q'] = 'Error estimate of integrated intensity at q'
         self.input_src['image_data'] = optools.wf_input
         self.input_src['wxd_dict'] = optools.wf_input
         self.categories = ['PROCESSING']
@@ -49,17 +60,21 @@ class ReduceByWXDDict(Operation):
         # initialization parameters, change into Fit2D format
         x0 = self.inputs['wxd_dict']['x0_pixel']
         y0 = self.inputs['wxd_dict']['y0_pixel']
-        s = int(img.shape[0])
+        # PyFAI magic go!
         p = pyFAI.AzimuthalIntegrator(wavelength=l)
         p.setFit2D(d,x0,y0,tilt,rot,pxsz,pxsz)
         # define detector mask, to screen bad pixels
         # should eventually be read in from dezinger output or something
         # for now just screen negative pixels
         # 1 for masked pixels, and 0 for valid pixels
-        detector_mask = np.ones((s,s))*(img <= 0)
+        if self.inputs['mask'] is None:
+            detector_mask = np.ones(img.shape)*(img <= 0)
+        else:
+            detector_mask = self.inputs['mask']
         q, I_of_q = p.integrate1d(img, 1000, mask=detector_mask, polarization_factor=fpolz)
         q = q * 1E9
         # save results to self.outputs
         self.outputs['q'] = q
-        self.outputs['I_of_q'] = I_of_q 
+        self.outputs['I_of_q'] = I_of_q
+        self.outputs['dI_of_q'] = None # placeholder
 
