31caaae685cecfe71d3c22be36152f0b4251ca39
Lenson Pellouchoud <lensonp@gmail.com>
2017-02-01 12:18:27 -0800

    major lean-out on plotmaker modules

--- slacx/slacxui/plotmaker_pqg.py
+++ slacx/slacxui/plotmaker_pqg.py
@@ -1,39 +1,4 @@
-import re
-
-import numpy as np
-from PySide import QtGui
 import pyqtgraph as pg
-import matplotlib
-from matplotlib.figure import Figure
-from matplotlib.backends import qt_compat
-from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigCanvas
-
-from . import uitools
-
-def display_item(itm,uri,qlayout,logmethod=None):
-    if type(itm).__name__ == 'ndarray':
-        dims = np.shape(itm)
-        if len(dims) == 2 and dims[0] > 2 and dims[1] > 2:
-            plot_widget = array_plot_2d(itm)
-        elif len(dims) == 1:
-            plot_widget = array_plot_1d(itm)
-    elif type(itm).__name__ == 'Figure':
-        plot_widget = plot_mpl_fig(itm)
-    else:
-        plot_widget = None
-    if plot_widget:
-        n_val_widgets = qlayout.count()
-        # Loop through them, last to first, clear the frame
-        for i in range(n_val_widgets-1,-1,-1):
-            # QLayout.takeAt returns a LayoutItem
-            widg = qlayout.takeAt(i)
-            # get the QWidget of that LayoutItem and set it to deleteLater()
-            widg.widget().deleteLater()
-        qlayout.addWidget(plot_widget,0,0,1,1) 
-    else:
-        # TODO: dialog box: tell user the selected item is uninteresting.
-        print '[{}]: selected item ({}) has no display method'.format(__name__,type(itm).__name__)
-        pass
 
 def array_plot_2d(data_in):
     return pqg_array_plot_2d(data_in)
