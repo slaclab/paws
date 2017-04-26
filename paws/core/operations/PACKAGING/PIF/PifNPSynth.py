7d4e77149d531208b808a7ab96784d522550e272
Lenson Pellouchoud <lensonp@gmail.com>
2017-04-25 17:46:41 -0700

    nanoparticle syntheses pif update

--- paws/core/operations/PACKAGING/PIF/PifNPSynth.py
+++ paws/core/operations/PACKAGING/PIF/PifNPSynth.py
@@ -10,37 +10,33 @@ class PifNPSynth(Operation):
     """
 
     def __init__(self):
-        input_names = ['name','q','I','date_time','t_utc','T']
+        input_names = ['uid_prefix','q_I','date_time','t_utc','T']
         output_names = ['pif']
         super(PifNPSynth,self).__init__(input_names,output_names)
-        self.input_doc['name'] = 'user input string used as pif record uid'
-        self.input_doc['q'] = 'array of q values for saxs spectrum'
-        self.input_doc['I'] = 'array of I(q) for saxs spectrum'
+        self.input_doc['uid_prefix'] = 'text string to prepend to pif uid (pif uid = uid_prefix+t_utc'
+        self.input_doc['q_I'] = 'n-by-2 array of q values and corresponding intensities for saxs spectrum'
         self.input_doc['date_time'] = 'string date/time from measurement header file for pif record tags'
         self.input_doc['t_utc'] = 'time in seconds utc'
         self.input_doc['T'] = 'temperature in degrees celsius from measurement header file'
         self.output_doc['pif'] = 'pif object containing the relevant data for this experiment'
-        self.input_src['name'] = optools.text_input
-        self.input_src['q'] = optools.wf_input
-        self.input_src['I'] = optools.wf_input
+        self.input_src['uid_prefix'] = optools.text_input
+        self.input_src['q_I'] = optools.wf_input
         self.input_src['date_time'] = optools.wf_input
         self.input_src['t_utc'] = optools.wf_input
         self.input_src['T'] = optools.wf_input
-        self.input_type['name'] = optools.str_type
-        self.input_type['q'] = optools.ref_type
-        self.input_type['I'] = optools.ref_type
+        self.input_type['uid_prefix'] = optools.str_type
+        self.input_type['q_I'] = optools.ref_type
         self.input_type['date_time'] = optools.ref_type
         self.input_type['t_utc'] = optools.ref_type
         self.input_type['T'] = optools.ref_type
 
     def run(self):
-        uid_pre = self.inputs['name']
+        uid_pre = self.inputs['uid_prefix']
         t_str = self.inputs['date_time']
         t_utc = self.inputs['t_utc']
         uid_full = uid_pre+'_'+str(int(t_utc))
         T_C = self.inputs['T']
-        q = self.inputs['q']
-        I_q = self.inputs['I']
+        q_I = self.inputs['q_I']
         # Subsystems for solution ingredients
         colloid_sys = pifobj.ChemicalSystem(uid_pre+'_pd_colloid',['colloidal Pd nanoparticles'],None,None,None,'Pd') 
         acid_sys = pifobj.ChemicalSystem(uid_pre+'_oleic_acid',['oleic acid'],None,None,None,'C18H34O2') 
@@ -51,22 +47,27 @@ class PifNPSynth(Operation):
         main_sys = pifobj.ChemicalSystem()
         main_sys.uid = uid_full
         main_sys.sub_systems = subsys
-        main_sys.properties = self.saxs_to_pif_properties(q,I_q,T_C)
+        main_sys.properties = self.saxs_to_pif_properties(q_I,T_C)
         main_sys.tags = ['reaction id: '+uid_pre,'date: '+t_str,'utc: '+str(int(t_utc))]
         self.outputs['pif'] = main_sys
 
-    def saxs_to_pif_properties(self,q,I_q,T_C):
-        props = []
-        for i in range(len(q)):
-            p = pifobj.Property()
-            p.name = 'SAXS intensity'
-            p.scalars = [ pifobj.Scalar(I_q[i]) ]
-            p.conditions = [] 
-            p.conditions.append( pifobj.Value('scattering vector',[pifobj.Scalar(q[i])],None,None,'Angstrom^-1') )
-            p.conditions.append( pifobj.Value('temperature',[pifobj.Scalar(T_C)],None,None,'degrees Celsius') )
-            p.units = 'counts'
-            props.append(p)
-        return props
+    def saxs_to_pif_properties(self,q_I,T_C):
+        #props = []
+        #for i in range(len(q)):
+        pq = pifobj.Property()
+        n_qpoints = q_I.shape[0]
+        ### property 1: scattering vector
+        pq.name = 'SAXS scattering vector'
+        pq.scalars = [pifobj.Scalar(q_I[i,0]) for i in range(n_qpoints)]
+        pq.conditions = [pifobj.Value('temperature',[pifobj.Scalar(T_C)],None,None,'degrees Celsius')]
+        pq.units = '1/Angstrom'
+        ### property 2: scattered intensity
+        pI = pifobj.Property()
+        pI.name = 'SAXS intensity'
+        pI.scalars = [pifobj.Scalar(q_I[i,1]) for i in range(n_qpoints)]
+        pI.conditions = [pifobj.Value('temperature',[pifobj.Scalar(T_C)],None,None,'degrees Celsius')]
+        pI.units = 'counts'
+        return [pq,pI] 
         
 #    def make_piftemperature(self,t):
 #        v = pifobj.Value()
