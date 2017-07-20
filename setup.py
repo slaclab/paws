cbb72694b93cee98abadaee5d00f3038e832e9ed
Lenson <lenson@slac.stanford.edu>
2017-07-20 12:56:44 -0700

    version+, setup dependencies cleaned

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pyyaml','pyside'],
+    install_requires=['pyyaml'],
     python_requires='>=2.6, <3',
 
     # List additional groups of dependencies here (e.g. development
