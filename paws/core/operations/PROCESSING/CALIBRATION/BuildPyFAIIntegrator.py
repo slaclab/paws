44bb4e1841eb23e3a0e3d7b8366f1074a7b210b7
Lenson Pellouchoud <lensonp@gmail.com>
2017-08-03 08:05:55 -0700

    WIP: detailed pyfai integration ops for xicam backend

---
+++ paws/core/operations/PROCESSING/CALIBRATION/BuildPyFAIIntegrator.py
@@ -0,0 +1,33 @@
+"""
+Produce a PyFAI.AzumthalIntegrator to use for calibrating and integrating images.
+"""
+
+import numpy as np
+import pyFAI
+
+from ... import Operation as opmod 
+from ...Operation import Operation
+
+class BuildPyFAIIntegrator(Operation):
+    """
+    Input dict of calibration parameters 
+    Return AzimuthalIntegrator 
+    """
+    def __init__(self):
+        input_names = ['poni_dict']
+        output_names = ['integrator']
+        super(BuildPyFAIIntegrator,self).__init__(input_names,output_names)
+        self.input_doc['poni_dict'] = str( 'dict of calibration parameters; '
+        + 'minimally including keys dist, poni1, poni2, rot1, rot2, rot3, pixel1, pixel2, wavelength;'
+        + 'optionally including keys fpolz, detector, splineFile; '
+        + 'same specifications as pyFAI .poni format calibration parameters')
+        self.input_src['poni_dict'] = opmod.wf_input
+        self.input_type['poni_dict'] = opmod.ref_type
+        self.output_doc['integrator'] = 'PyFAI.AzimuthalIntegrator object set up with input poni_dict'
+
+    def run(self):
+        pd = self.inputs['poni_dict']
+        p = pyFAI.AzimuthalIntegrator()
+        p.setPyFAI(**pd)
+        self.outputs['integrator'] = p
+
