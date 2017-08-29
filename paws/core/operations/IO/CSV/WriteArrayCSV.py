026b0b4c58f9165d0b6418083574114cadafc754
Lenson Pellouchoud <lensonp@gmail.com>
2017-08-28 22:12:02 -0700

    minor change in op input name for consistency between similar ops

--- paws/core/operations/IO/CSV/WriteArrayCSV.py
+++ paws/core/operations/IO/CSV/WriteArrayCSV.py
@@ -10,14 +10,14 @@ class WriteArrayCSV(Operation):
 
     def __init__(self):
         input_names = ['array','headers','dir_path','filename','filetag']
-        output_names = ['csv_path']
+        output_names = ['file_path']
         super(WriteArrayCSV, self).__init__(input_names, output_names)
         self.input_doc['array'] = 'any 2d array'
         self.input_doc['headers'] = 'list of string headers (optional)- one header for each column of array'
         self.input_doc['dir_path'] = 'the path to the destination directory'
         self.input_doc['filename'] = 'the name of the file to be saved- no extension is expected'
         self.input_doc['filetag'] = 'tag appended to filename- no extension is expected'
-        self.output_doc['csv_path'] = 'the path to the finished csv file: dir_path+filename+filetag+.csv'
+        self.output_doc['file_path'] = 'the path to the finished csv file: dir_path+filename+filetag+.csv'
         self.input_type['array'] = opmod.workflow_item
         self.inputs['filetag'] = ''
 
@@ -29,7 +29,7 @@ class WriteArrayCSV(Operation):
         if self.inputs['filetag']:
             tag = self.inputs['filetag']
         csv_path = self.inputs['dir_path']+'/'+self.inputs['filename']+tag+'.csv'
-        self.outputs['csv_path'] = csv_path
+        self.outputs['file_path'] = csv_path
         if h is not None:
             h_str = ''
             for i in range(len(h)-1):
