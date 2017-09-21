85a1597c1815122199b4af18777b0e92385ce9ec
Lenson Pellouchoud <lensonp@gmail.com>
2017-09-21 15:58:54 -0700

    ops test error handling

--- tests/test_ops.py
+++ tests/test_ops.py
@@ -7,7 +7,12 @@ class TestOps(unittest.TestCase):
     def test_activate_ops(self):
         paw = paws.api.start()
         for op_uri in paw._op_manager.list_ops():
-            paw.activate_op(op_uri) 
+            try:
+                paw.activate_op(op_uri) 
+            except ImportError:
+                self.skipTest(u'Caught ImportError: '\
+                'Python environment is not ready for {}'\
+                .format(op_uri))
 
 if __name__ == '__main__':
     unittest.main()
