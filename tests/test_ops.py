2ac106296643e1f7fa2255fe6124243e6c0bd7bf
Lenson Pellouchoud <lensonp@gmail.com>
2017-09-21 13:17:44 -0700

    split api and ops tests

--- tests/test_ops.py
+++ tests/test_ops.py
@@ -4,10 +4,10 @@ import paws.api
 
 class TestAPI(unittest.TestCase):
 
-    def test_start_stop(self):
-        # smoke testing paws api
+    def test_activate_ops(self):
         paw = paws.api.start()
-        self.assertIsInstance(paw,paws.api.PawsAPI)
+        for op_uri in paw._op_manager.list_ops():
+            paw.activate_op(op_uri) 
 
 if __name__ == '__main__':
     unittest.main()
