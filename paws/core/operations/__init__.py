00634c34eb1d1c9df0a7ee9cdc3604a10f596b99
Lenson Pellouchoud <lensonp@gmail.com>
2017-09-21 15:42:11 -0700

    better op loading dict updates in operation.__init__

--- paws/core/operations/__init__.py
+++ paws/core/operations/__init__.py
@@ -31,11 +31,6 @@ def save_config():
         od_load_flags[k] = load_flags[k]
     pawstools.save_cfg(od_load_flags,cfg_file)
 
-def update_load_flags():
-    for k in load_flags.keys():
-        if not k in load_keys:
-            load_flags.pop(k)
-
 def load_ops_from_path(path_,pkg,cat_root=''):
     ops = []
     cats = []
@@ -56,9 +51,6 @@ def load_ops_from_path(path_,pkg,cat_root=''):
             # NOTE: This line determines whether or not 
             # newly arrived modules should be loaded by default.
             load_mod = False
-        #
-        #load_mod = True
-        #
         load_flags[mod_root] = load_mod
         # if it is a package, recurse
         if ispkg:
@@ -75,24 +67,6 @@ def load_ops_from_path(path_,pkg,cat_root=''):
                 cats.append(cat_root)
             ops.append( (cat_root,modname) )
     return ops, cats
-        #if load_mod:
-        #    # Assume Operation name is same as module name.
-        #    # Get the operation from the module.
-        #    # TODO: Handle any ImportErrors or AttributeErrors.
-        #    mod = importlib.import_module('.'+modname,pkg)
-        #    op = getattr(mod,modname)
-            #try:
-            #    ops.append( (cat_root,op) )
-            #    if not cat_root in cats:
-            #        cats.append(cat_root)
-            #except AttributeError as ex:
-            #    pass
-            #    msg = str('Failed to load Operation subclass {} '.format(modname)
-            #    + 'from module of the same name. Error message: '+ ex.message
-            #    + '\nTo load an Operation, '
-            #    + 'ensure the Operation subclass '
-            #    + 'has the same name as its .py module file')
-            #    print(msg)
 
 def disable_ops(disable_root):
     # get all keys that contain disable_root
@@ -100,55 +74,7 @@ def disable_ops(disable_root):
     for k in disable_keys:
         load_flags[k] = False 
 
-#print(load_flags)
-
 cat_op_list, cat_list = load_ops_from_path(__path__,__name__)
 
-update_load_flags()
-
-#op = load_op_from_module(mod,cat_root)
-#mod_ops, mod_cats = load_ops_from_module(mod,cat_root)
-#mod_ops = [op for op in mod_ops if not op in ops]
-#mod_cats = [cat for cat in mod_cats if not cat in cats]
-#ops = ops + mod_ops
-#cats = cats + mod_cats
-
-#def load_op_from_module(mod,cat_root):
-    # Get the operation from the module:
-    # assume Operation name is same as module name
-    #op = getattr(mod,mod)
-    #ops = []
-    #cats = []
-    #for nm, itm in mod.__dict__.items():
-    #    try:
-    #        # is it a class?
-    #        if isinstance(itm,type):
-    #            # is it a non-abstract subclass of Operation?
-    #            if issubclass(itm,Operation) and not nm in ['Operation','Realtime','Batch']:
-    #                op = getattr(mod,nm)
-    #                ops.append( (cat_root,op) )
-    #                if not cat_root in cats:
-    #                    cats.append(cat_root)
-    #                    #load_flags[cat_root] = True
-    #                    #load_keys.append(cat_root)
-    #                #cat = cat_root
-    #                parent_cats_done = False
-    #                while not parent_cats_done:
-    #                    if not cat.rfind('.') == -1:
-    #                        parcat = cat[:cat.rfind('.')]
-    #                        if not parcat in cats:
-    #                            cats.append(parcat)
-    #                            load_keys.append(parcat)
-    #                        cat = parcat
-    #                    else:
-    #                        parent_cats_done = True
-    #                load_flags[nm] = True
-    #                load_keys.append(nm)
-    #    except ImportError as ex:
-    #        print '[{}] had trouble dealing with {}: {}'.format(__name__,name,item)
-    #        print 'Error text: {}'.format(ex.message)
-    #        pass 
-    #return ops, cats
-
-
+load_flags = {k:load_flags[k] for k in load_flags.keys() if k in load_keys}
 
