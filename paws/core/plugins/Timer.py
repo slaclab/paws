4fec2cd131688616440e80332e2f7f8ab91722d5
Lenson Pellouchoud <lensonp@gmail.com>
2018-02-23 16:28:00 -0800

    simplified Timer plugin, py2 and py3 tested, py2 wait() has to check for own timeout condition

--- paws/core/plugins/Timer.py
+++ paws/core/plugins/Timer.py
@@ -4,7 +4,7 @@ from .PawsPlugin import PawsPlugin
 import datetime
 import time
 import sys
-from threading import Event, Condition
+from threading import Condition
 
 import tzlocal
 import numpy as np
@@ -18,53 +18,46 @@ class Timer(PawsPlugin):
         super(Timer,self).__init__(inputs)
         self.input_doc['dt'] = 'Wait time between signals, in seconds'
         self.input_doc['t_max'] = 'Time in seconds after which the timer stops'
-        self.clock = Event()
+        #self.clock = Event()
+        self.tz = tzlocal.get_localzone()
+        self.ep = datetime.datetime.fromtimestamp(0,self.tz)
         self.dt_lock = Condition()
+        #self.dt_lock.acquire()
+        self.t0 = None 
+        self.t0_utc = None 
+        self.dt_now = 0.
         self.time_points = []
         self.content = OrderedDict(
-            clock=self.clock,
             time_points=self.time_points)
 
     def start(self):
         self.dt = self.inputs['dt'] 
-        self.t_max = self.inputs['t_max'] 
-        self.tz = tzlocal.get_localzone()
-        self.ep = datetime.datetime.fromtimestamp(0,self.tz)
+        self.t_max = self.inputs['t_max']
+        # set zero-time point 
         self.t0 = datetime.datetime.now(self.tz)
         self.t0_utc = (self.t0-self.ep).total_seconds()
-        # lock dt_utc while it is changed 
-        self.dt_lock.acquire()
-        # tick the clock
-        self.clock.set()
-        self.clock.clear()
-        self.dt_now = self.dt_utc()
-        # notify listeners that time has advanced
-        self.dt_lock.notify()
-        # release the lock
-        self.dt_lock.release()
-        self.time_points.append(self.dt_now)
-        self.running = True
-        while self.running:
-            #time.sleep(self.dt)
-            t_rem = self.dt-np.mod(self.dt_utc(),self.dt)
-            time.sleep(t_rem)
-            # before the clock ticks, lock dt_utc
-            self.dt_lock.acquire()
-            # tick the clock
-            self.clock.set()
-            self.clock.clear()
+        # acquire lock, set initial time point, notify, release
+        with self.dt_lock:
             self.dt_now = self.dt_utc()
-            # notify listeners that time has advanced
             if int(sys.version[0]) == 2:
                 self.dt_lock.notifyAll()
             else:
                 self.dt_lock.notify_all()
-            # release the lock
-            self.dt_lock.release()
             self.time_points.append(self.dt_now)
+        self.running = True
+        while self.running:
+            # attempt to nail the next dt point:
+            t_rem = self.dt-np.mod(self.dt_utc(),self.dt)
+            time.sleep(t_rem)
+            with self.dt_lock:
+                self.dt_now = self.dt_utc()
+                if int(sys.version[0]) == 2:
+                    self.dt_lock.notifyAll()
+                else:
+                    self.dt_lock.notify_all()
+                self.time_points.append(self.dt_now)
             if self.dt_now > self.t_max:
                 self.running = False
-        print('timer is finished.')
 
     def dt_utc(self):
         t = datetime.datetime.now(self.tz)
