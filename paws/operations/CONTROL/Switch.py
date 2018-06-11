dc04d9fa7589668613ce1d2c32d7500a24c5eef0
Lenson Pellouchoud <lensonp@gmail.com>
2018-06-11 16:51:46 -0700

    CONTROL.Switch Operation

---
+++ paws/operations/CONTROL/Switch.py
@@ -0,0 +1,33 @@
+from collections import OrderedDict
+
+from ..Operation import Operation
+
+inputs=OrderedDict(
+    iftrue_data=None,
+    else_data=None,
+    condition=None,
+    truth_condition=None)
+
+outputs=OrderedDict(
+    data=None)
+        
+class Switch(Operation):
+    """Output one of two different inputs depending on an input condition"""
+
+    def __init__(self):
+        super(Switch,self).__init__(inputs,outputs)
+        self.input_doc['iftrue_data'] = 'the output value if `condition`==`truth_condition`'
+        self.input_doc['else_data'] = 'the output value if not `condition`==`truth_condition`'
+        self.input_doc['condition'] = 'condition for deciding whether or not to run'
+        self.input_doc['truth_condition'] = 'value to compare against `condition`'
+        self.output_doc['data'] = 'the piece of data selected by the `condition`'
+
+    def run(self):
+        cond = self.inputs['condition']
+        tcond = self.inputs['truth_condition']
+        if cond == rcond: 
+            self.outputs['data'] = self.inputs['iftrue_data'] 
+        else:
+            self.outputs['data'] = self.inputs['else_data'] 
+        self.outputs['outputs'] = out_dict
+
