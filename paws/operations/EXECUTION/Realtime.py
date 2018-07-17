07cb8d4aa19e589207a5e66960d6f2f30ba2e5aa
Lenson Pellouchoud <lensonp@gmail.com>
2018-07-16 17:11:39 -0700

    updated EXECUTION ops to better use Workflow as DictTree

--- paws/operations/EXECUTION/Realtime.py
+++ paws/operations/EXECUTION/Realtime.py
@@ -7,11 +7,9 @@ from .. import optools
 
 inputs=OrderedDict(
     work_item=None,
-    input_generators=[],
-    input_keys=[],
-    static_inputs=[],
-    static_input_keys=[],
-    delay=1.,
+    input_generators={},
+    static_inputs={},
+    wait_delay=1.,
     max_delay=float('inf'),
     max_exec=float('inf'))
 
@@ -27,101 +25,102 @@ class Realtime(Operation):
         self.input_doc['work_item'] = 'the Operation '\
             'or Workflow object to be executed in real time'
 
-        self.input_doc['input_generators'] = 'one generator for each of '\
-            'the `input_keys`, to generate inputs during execution- '\
-            '`work_item` will be executed when all of the generators '\
-            'return a value that is not None'
-        self.input_doc['input_keys'] = 'list of keys for setting '\
-            'inputs of the `work_item`- these should correspond to '\
-            'either Operation.inputs or Workflow.inputs, '\
-            'depending on whether `work_item` is an Operation or a Workflow'
+        self.input_doc['input_generators'] = 'dict of generators, '\
+            'where the dict keys refer to `work_item` inputs, '\
+            'and values are generators whose values are used '\
+            'as inputs for each iteration of `work_item`.'
+        self.input_doc['static_inputs'] = 'similar to `input_generators`, '\
+            'but with single values instead of generators- '\
+            'the same value is used as input on each iteration.'
 
-        self.input_doc['static_inputs'] = 'one object for each of '\
-            'the `work_item` inputs indicated by `static_input_keys`, '\
-            'to be set to the same value for every execution'
-        self.input_doc['static_input_keys'] = 'list of keys for setting '\
-            'static inputs of the `work_item`- these should correspond to '\
-            'either Operation.inputs or Workflow.inputs, '\
-            'depending on whether `work_item` is an Operation or a Workflow'
-            
-        self.input_doc['delay'] = 'delay in seconds '\
+        self.input_doc['wait_delay'] = 'delay in seconds '\
             'between attempts to generate new inputs'
         self.input_doc['max_delay'] = '(optional) maximum delay'\
             'in seconds, before giving up and stopping execution'
         self.input_doc['max_exec'] = '(optional) maximum number '\
             'of executions of `work_item`'
             
-        self.output_doc['realtime_inputs'] = 'list of dicts '\
-            'containing [input_name:input_value] '\
-            'for each execution of `work_item`'
-        self.output_doc['realtime_outputs'] = 'list of dicts '\
-            'containing [output_name:output_value] '\
-            'for each execution of `work_item`'
+        self.output_doc['realtime_inputs'] = 'dict of lists, '\
+            'where each list contains the realtime inputs '\
+            'for the corresponding input key.'
+        self.output_doc['realtime_outputs'] = 'dict of lists, '\
+            'where each list contains the realtime outputs '\
+            'for the corresponding output key.'
         
     def run(self):
         wrk = self.inputs['work_item']
         inp_gens = self.inputs['input_generators']
-        inpks = self.inputs['input_keys']
         stat_inps = self.inputs['static_inputs']
-        stat_inpks = self.inputs['static_input_keys']
         maxdly = self.inputs['max_delay']
         maxexec = self.inputs['max_exec']
-        dly = self.inputs['delay']
+        dly = self.inputs['wait_delay']
 
-        self.outputs['realtime_inputs'] = [] 
-        self.outputs['realtime_outputs'] = [] 
+        ins_dict = dict.fromkeys(inp_gens.keys()) 
+        outs_dict = dict.fromkeys(wrk.outputs.keys()) 
+        for k in inp_gens.keys():
+            ins_dict[k] = []
+        for k in wrk.outputs.keys():
+            outs_dict[k] = [] 
+        self.outputs['realtime_inputs'] = ins_dict 
+        self.outputs['realtime_outputs'] = outs_dict 
         if self.data_callback: 
-            self.data_callback('outputs.realtime_inputs',[])
-            self.data_callback('outputs.realtime_outputs',[])
+            self.data_callback('outputs.realtime_inputs',ins_dict)
+            self.data_callback('outputs.realtime_outputs',outs_dict)
 
         nx = 0 # total number of executions
         keep_going = True
+        if maxexec < 1: keep_going = False
         self.message_callback('STARTING REALTIME EXECUTION')
         while keep_going:
-            # collect inputs from inp_gens
-            inp_dict = OrderedDict.fromkeys(inpks)
+            gen_inp_dict = OrderedDict.fromkeys(inp_gens)
             inps_ready = False
             nd = 0 # number of consecutive delays
-            if nx >= maxexec:
-                msg = 'Reached maximum executions ({}): Realtime stopping.'.format(maxexec) 
-                self.message_callback(msg)
-                keep_going = False
-            while keep_going and not inps_ready:
+            while not inps_ready:
 
+                # handle stop requests while waiting for new inputs
                 if self.stop_flag:
                     self.message_callback('Realtime execution stopped.')
                     return
                 
-                for k,gen in zip(inpks,inp_gens):
-                    if inp_dict[k] is None:
-                        inp_dict[k] = gen.next()
-                if all([inp_dict[k] is not None for k in inpks]):
-                    inps_ready = True
-                    if self.data_callback: 
-                        self.data_callback('outputs.realtime_inputs.'+str(nx),copy.deepcopy(inp_dict))
-                else:
-                    # delay
+                inps_ready = True
+                for k,gen in inp_gens.items():
+                    if gen_inp_dict[k] is None:
+                        gen_inp_dict[k] = next(gen)
+                    if gen_inp_dict[k] is None:
+                        inps_ready = False 
+                if not inps_ready: 
                     time.sleep(float(dly)) 
                     nd+=1
                     currentdly = nd*dly
-                    #self.message_callback('... WAITING FOR INPUTS ({}/{} ms)'
-                    #    .format(currentdly,maxdly))
                     if currentdly >= maxdly:
                         msg = 'Reached maximum delay ({}): Realtime stopping.'.format(maxdly) 
                         self.message_callback(msg)
                         keep_going = False 
+
             if keep_going:
-                for inpnm,inpval in inp_dict.items():
+                for inpnm,inpval in gen_inp_dict.items():
                     wrk.set_input(inpnm,inpval)
-                if any(stat_inpks): 
-                    for inpnm,inpval in zip(stat_inpks,stat_inps):
-                        wrk.set_input(inpnm,inpval)
+                    ins_dict[inpnm].append(inpval)
+                for inpnm,inpval in stat_inps.items():
+                    wrk.set_input(inpnm,inpval)
+                if self.data_callback:
+                    for k,val in gen_inp_dict.items():
+                        self.data_callback('outputs.realtime_inputs.'+k+'.'+str(nx),val)
                 self.message_callback('REALTIME RUN {}'.format(nx))
                 wrk.run()
                 out_dict = wrk.get_outputs()
-                self.outputs['realtime_outputs'].append(out_dict)
+                for k,v in out_dict.items():
+                    outs_dict[k].append(v)
                 if self.data_callback: 
-                    self.data_callback('outputs.realtime_outputs.'+str(nx),copy.deepcopy(out_dict))
+                    for k,val in out_dict.items():
+                        self.data_callback('outputs.realtime_inputs.'+k+'.'+str(nx),val)
                 nx+=1
+            if nx >= maxexec:
+                msg = 'Reached maximum executions ({}): Realtime stopping.'.format(maxexec) 
+                self.message_callback(msg)
+                keep_going = False
+            if self.stop_flag:
+                self.message_callback('Realtime loop stopped.')
+                keep_going = False 
         self.message_callback('REALTIME EXECUTION STOPPED')
 
--- paws/operations/EXECUTION/Run.py
+++ paws/operations/EXECUTION/Run.py
@@ -9,8 +9,7 @@ from .. import optools
 
 inputs=OrderedDict(
     work_item=None,
-    inputs=None,
-    input_keys=None)
+    inputs={})
 
 outputs=OrderedDict(outputs=None)
 
@@ -21,25 +20,17 @@ class Run(Operation):
         super(Run,self).__init__(inputs,outputs)
         self.input_doc['work_item'] = 'the Operation '\
             'or Workflow object to be executed'
-        self.input_doc['inputs'] = 'one object for each of '\
-            'the `work_item` inputs indicated by `input_keys`.'
-        self.input_doc['input_keys'] = 'list of keys for setting '\
-            'inputs of the `work_item`.'
-        self.output_doc['outputs'] = 'if `run_flag` is True, '\
-            'this will hold the `work_item` outputs'
-        self.input_datatype['input_keys'] = 'list'
-        self.input_datatype['inputs'] = 'list'
+        self.input_doc['inputs'] = 'dict of inputs to add to `work_item`'
+        self.output_doc['outputs'] = '`work_item` outputs'
+        self.input_datatype['inputs'] = dict 
 
     def run(self):
         wrk = self.inputs['work_item']
-        inpks = self.inputs['input_keys']
-        inpvals = self.inputs['inputs']
-
-        if any(inpks): 
-            for inpk,inpval in zip(inpks,inpvals):
+        inps = self.inputs['inputs']
+        if any(inps): 
+            for inpk,inpval in inps.items():
                 wrk.set_input(inpk,inpval)
         wrk.run()
         out_dict = wrk.get_outputs()
-
         self.outputs['outputs'] = out_dict
 
