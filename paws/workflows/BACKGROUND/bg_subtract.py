400a7bb98e5b08e1248958c78c5d7d5472e1055f
Lenson Pellouchoud <lensonp@gmail.com>
2018-07-16 17:37:20 -0700

    modular workflows for reading, background subtracting, fitting

--- paws/workflows/BACKGROUND/bg_subtract.py
+++ paws/workflows/BACKGROUND/bg_subtract.py
@@ -8,35 +8,40 @@ wfmgr = WfManager()
 
 wfmgr.add_workflow('bg_subtract')
 wfmgr.load_operations('bg_subtract',
-    read_bg = 'IO.NumpyLoad',
-    read_data = 'IO.NumpyLoad',
+    read_q_I = 'IO.NumpyLoad',
+    read_q_I_bg = 'IO.NumpyLoad',
     subtract_bg = 'PROCESSING.BACKGROUND.BgSubtract',
-    output_path = 'IO.FILESYSTEM.BuildFilePath',
-    write_q_I = 'IO.NumpySave'
+    output_file = 'IO.FILESYSTEM.BuildFilePath',
+    write_q_I_bgsub = 'IO.NumpySave'
     )
 
 wf = wfmgr.workflows['bg_subtract']
-# inputs: paths to data and bg files
-wf.connect_input('bg_file','read_bg.inputs.file_path')
-wf.connect_input('data_file','read_data.inputs.file_path')
-# inputs: data and bg data (for skipping file io) 
-wf.connect_input('bg_data','read_bg.outputs.data')
-wf.connect_input('data','read_data.outputs.data')
 
+wf.connect_input('q_I_file','read_q_I.inputs.file_path')
+wf.connect_input('q_I_bg_file','read_q_I_bg.inputs.file_path')
+wf.connect('read_q_I.outputs.data','subtract_bg.inputs.q_I')
+wf.connect('read_q_I_bg.outputs.data','subtract_bg.inputs.q_I_bg')
+
+# connect inputs for bypassing IO
+#wf.connect_input('q_I_bg','read_q_I_bg.outputs.data')
+#wf.connect_input('q_I','read_q_I.outputs.data')
+wf.connect_input('q_I','subtract_bg.inputs.q_I')
+wf.connect_input('q_I_bg','subtract_bg.inputs.q_I_bg')
 wf.connect_output('bg_factor','subtract_bg.outputs.bg_factor')
 wf.connect_output('q_I_bgsub','subtract_bg.outputs.q_I_bgsub')
 
-wf.connect('read_data.outputs.data','subtract_bg.inputs.q_I')
-wf.connect('read_bg.outputs.data','subtract_bg.inputs.q_I_bg')
+wf.connect_input('output_dir','output_file.inputs.dir_path')
+wf.connect_input('output_filename','output_file.inputs.filename')
+wf.connect('read_q_I.outputs.filename','output_file.inputs.filename')
+wf.connect('read_q_I.outputs.dir_path','output_file.inputs.dir_path')
+wf.set_op_input('output_file','suffix','_bgsub')
+wf.set_op_input('output_file','extension','dat')
+wf.connect_output('q_I_file','output_file.outputs.file_path')
+wf.connect_output('q_I_filename','output_file.outputs.filename')
 
-wf.connect('read_data.outputs.dir_path','output_path.inputs.dir_path')
-wf.connect('read_data.outputs.filename','output_path.inputs.filename')
-wf.set_op_input('output_path','suffix','_bgsub')
-wf.set_op_input('output_path','extension','dat')
-
-wf.connect('subtract_bg.outputs.q_I_bgsub','write_q_I.inputs.data')
-wf.set_op_input('write_q_I','header','q (1/angstrom), Intensity (arb)')
-wf.connect('output_path.outputs.file_path','write_q_I.inputs.file_path')
+wf.connect('subtract_bg.outputs.q_I_bgsub','write_q_I_bgsub.inputs.data')
+wf.set_op_input('write_q_I_bgsub','header','q (1/angstrom), Intensity (arb)')
+wf.connect('output_file.outputs.file_path','write_q_I_bgsub.inputs.file_path')
 
 wfmgr.save_to_wfl('bg_subtract',os.path.join(pawstools.sourcedir,'workflows','BACKGROUND','bg_subtract.wfl'))
 
--- paws/workflows/IO/BL15/read_legacy.py
+++ paws/workflows/IO/BL15/read_legacy.py
@@ -5,22 +5,23 @@ from paws.workflows.WfManager import WfManager
 
 wfmgr = WfManager()
 
-wfmgr.add_workflow('read_header')
-wfmgr.load_operations('read_header',
+wfmgr.add_workflow('read')
+wfmgr.load_operations('read',
     read_header='IO.BL15.ReadHeader',
     time_temp='PACKAGING.BL15.TimeTempFromHeader',
     image_filepath='IO.FILESYSTEM.BuildFilePath',
-    data_filepath='IO.FILESYSTEM.BuildFilePath'
+    data_filepath='IO.FILESYSTEM.BuildFilePath',
+    read_image='IO.IMAGE.FabIOOpen',
+    read_q_I='IO.NumpyLoad'
     )
 
-wf = wfmgr.workflows['read_header']
+wf = wfmgr.workflows['read']
 
 # input: header file path
 wf.connect_input('header_filepath','read_header.inputs.file_path') 
 # inputs: keys for getting time and temperature from header dict
 wf.connect_input('time_key','time_temp.inputs.time_key') 
 wf.connect_input('temperature_key','time_temp.inputs.temperature_key') 
-wf.connect_input('image_dir','image_filepath.inputs.dir_path')
 # inputs: directories and suffixes for locating data files 
 wf.connect_input('image_dir','image_filepath.inputs.dir_path')
 wf.connect_input('data_dir','data_filepath.inputs.dir_path')
@@ -33,6 +34,8 @@ wf.connect_output('temperature','time_temp.outputs.temperature')
 wf.connect_output('filename','read_header.outputs.filename')
 wf.connect_output('image_filepath','image_filepath.outputs.file_path')
 wf.connect_output('data_filepath','data_filepath.outputs.file_path')
+wf.connect_output('image_data','read_image.outputs.image_data')
+wf.connect_output('q_I','read_q_I.outputs.data')
 
 wf.connect('read_header.outputs.header_dict','time_temp.inputs.header_dict')
 wf.set_op_input('time_temp','time_key','time')
@@ -44,6 +47,8 @@ wf.connect('read_header.outputs.filename',[\
 wf.set_op_input('image_filepath','extension','tif')
 wf.set_op_input('data_filepath','suffix','_dz_bgsub')
 wf.set_op_input('data_filepath','extension','dat')
+wf.connect('image_filepath.outputs.file_path','read_image.inputs.file_path')
+wf.connect('data_filepath.outputs.file_path','read_q_I.inputs.file_path')
 
-wfmgr.save_to_wfl('read_header',os.path.join(pawstools.sourcedir,'workflows','IO','BL15','read_header.wfl'))
+wfmgr.save_to_wfl('read',os.path.join(pawstools.sourcedir,'workflows','IO','BL15','read_legacy.wfl'))
 
