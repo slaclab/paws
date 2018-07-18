44f66eb886ba5f9bc22d15083411f98e75868e46
Lenson <lenson@slac.stanford.edu>
2018-07-17 18:02:23 -0700

    refactoring beam line 1-5 fitting workflows

--- paws/workflows/FITTING/BL15/timeseries_fit.py
+++ paws/workflows/FITTING/BL15/timeseries_fit.py
@@ -4,23 +4,14 @@ from paws import pawstools
 from paws.workflows.WfManager import WfManager
 
 wfmgr = WfManager()
-wfmgr.add_workflow('main')
-wfmgr.load_operations('main',
-    header_files='IO.FILESYSTEM.BuildFileList',
-    header_batch='EXECUTION.Batch',
-    t_filepaths='PACKAGING.BATCH.XYDataFromBatch',
-    t_filenames='PACKAGING.BATCH.XYDataFromBatch',
-    batch_fit='EXECUTION.Batch'
-    )
 
 wfmgr.load_packaged_wfm('FITTING.BL15.batch_fit')
 
 wf = wfmgr.workflows['main']
 
 wf.set_op_inputs('t_filenames',x_sort_flag=True)
-wf.set_op_inputs('t_filepaths',x_sort_flag=True)
-wf.connect('t_filenames.outputs.y','batch_fit.inputs.batch_inputs.filename')
-wf.connect('t_filepaths.outputs.y','batch_fit.inputs.batch_inputs.data_filepath')
+wf.set_op_inputs('t_q_I_files',x_sort_flag=True)
+wf.set_op_inputs('t_populations_files',x_sort_flag=True)
 wf.set_op_input('batch_fit','serial_params',
     {'populations':'populations','param_bounds':'param_bounds',
     'fixed_params':'fixed_params','param_constraints':'param_constraints'})
