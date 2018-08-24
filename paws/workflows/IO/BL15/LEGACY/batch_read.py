0caa9ec56ecf89c4e99e3247afa94848ad637cdf
Lenson <lenson@slac.stanford.edu>
2018-08-24 11:28:07 -0700

    improvements to BL15 IO read and batch_read workflows

--- paws/workflows/IO/BL15/LEGACY/batch_read.py
+++ paws/workflows/IO/BL15/LEGACY/batch_read.py
@@ -5,21 +5,10 @@ from paws.workflows.WfManager import WfManager
 
 wfmgr = WfManager()
 
+wfmgr.load_packaged_wfm('IO.BL15.batch_read')
 wfmgr.load_packaged_workflow('read','IO.BL15.LEGACY.read')
-wfmgr.add_workflow('batch_read')
-wfmgr.load_operations('batch_read',
-    header_files='IO.FILESYSTEM.BuildFileList',
-    batch='EXECUTION.Batch',
-    )
 
-wf = wfmgr.workflows['batch_read']
-
-# input: header file path
-wf.connect_input('header_dir','header_files.inputs.dir_path') 
-# input: header files regex 
-wf.connect_input('header_regex','header_files.inputs.regex') 
-wf.connect_output('batch_outputs','batch_read.outputs.batch_outputs')
-wf.connect('header_files.outputs.file_list','batch.inputs.batch_inputs.header_filepath')
+wfmgr.workflows['batch_read'].connect_workflow('read','batch.inputs.work_item')
 
 wfmgr.save_to_wfm(os.path.join(pawstools.sourcedir,'workflows','IO','BL15','LEGACY','batch_read.wfm'))
 
