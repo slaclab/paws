801749695a0ebd0eddcb602953d1850578f2c06b
Lenson <lenson@slac.stanford.edu>
2018-11-21 15:38:47 -0800

    improved logic of BL15 Read workflow

--- paws/workflows/IO/BL15/Read.py
+++ paws/workflows/IO/BL15/Read.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+import copy
 import os
 
 from ...Workflow import Workflow 
@@ -39,53 +40,48 @@ class Read(Workflow):
             )
 
     def run(self):
+        self.outputs = copy.deepcopy(outputs)
 
         if self.ops_enabled['read_header']:
             if (self.inputs['header_file']) and (os.path.exists(self.inputs['header_file'])):
-                self.operations['read_header'].run_with(
+                hdr_outs = self.operations['read_header'].run_with(
                 file_path=self.inputs['header_file'])
+                hdata = hdr_outs['data']
+                self.outputs['header_data'] = hdata
+                if hdata and self.inputs['time_key']:
+                    self.outputs['time'] = hdata[self.inputs['time_key']] 
             else:
                 self.message_callback('header file not found: {}'.format(self.inputs['header_file']))
 
         if self.ops_enabled['read_image']:
             if (self.inputs['image_file']) and (os.path.exists(self.inputs['image_file'])):
-                self.operations['read_image'].run_with(
+                img_outs = self.operations['read_image'].run_with(
                 file_path=self.inputs['image_file'])
+                self.outputs['image_data'] = img_outs['image_data']
             else:
                 self.message_callback('image file not found: {}'.format(self.inputs['image_file']))
 
         if self.ops_enabled['read_q_I']:
             if (self.inputs['q_I_file']) and (os.path.exists(self.inputs['q_I_file'])):
-                self.operations['read_q_I'].run_with(
+                q_I_outs = self.operations['read_q_I'].run_with(
                 file_path=self.inputs['q_I_file'])
+                q_I = q_I_outs['data']
+                dI = None
+                if (q_I is not None) and (q_I.shape[1] > 2):
+                    q_I = q_I[:,:2]
+                    dI = q_I[:,2]
+                self.outputs['q_I'] = q_I
+                self.outputs['dI'] = dI
             else:
                 self.message_callback('q_I file not found: {}'.format(self.inputs['q_I_file']))
 
         if self.ops_enabled['read_system']:
             if (self.inputs['system_file']) and (os.path.exists(self.inputs['system_file'])):
-                self.operations['read_system'].run_with(
+                sys_outs = self.operations['read_system'].run_with(
                 file_path=self.inputs['system_file'])
+                self.outputs['system'] = sys_outs['system']
             else:
                 self.message_callback('xrsd system file not found: {}'.format(self.inputs['system_file']))
 
-        hdata = self.operations['read_header'].outputs['data']
-        if hdata and self.inputs['time_key']:
-           self.outputs['time'] = hdata[self.inputs['time_key']] 
-
-        q_I = None
-        dI = None
-        if self.ops_enabled['read_q_I']:
-            q_I = self.operations['read_q_I'].outputs['data']
-            if (q_I is not None) and (q_I.shape[1] > 2):
-                q_I = q_I[:,:2]
-                dI = q_I[:,2]
-
-        self.outputs.update(
-            header_data = hdata, 
-            image_data = self.operations['read_image'].outputs['image_data'],
-            q_I = q_I,
-            dI = dI,
-            system = self.operations['read_system'].outputs['system']
-            )
         return self.outputs
 
