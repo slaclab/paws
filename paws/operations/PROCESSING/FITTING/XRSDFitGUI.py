8a24c8bd6da1718364a82e31956e13b3507a2a57
Lenson <lenson@slac.stanford.edu>
2018-12-14 11:26:43 -0800

    good_fit flag is now embedded in System objects

--- paws/operations/PROCESSING/FITTING/XRSDFitGUI.py
+++ paws/operations/PROCESSING/FITTING/XRSDFitGUI.py
@@ -11,14 +11,12 @@ inputs = OrderedDict(
     system=None,
     error_weighted=True,
     logI_weighted=True,
-    q_range=[0.,float('inf')],
-    good_fit_prior=False)
+    q_range=[0.,float('inf')])
 
 outputs = OrderedDict(
     system=None,
     system_dict=None,
-    q_I_opt = None,
-    good_fit_flag=False) 
+    q_I_opt = None) 
 
 class XRSDFitGUI(Operation):
     """Interactively fit a XRSD spectrum."""
@@ -31,11 +29,9 @@ class XRSDFitGUI(Operation):
         self.input_doc['error_weighted'] = 'flag for using I(q) error estimates to weight fit objective'
         self.input_doc['logI_weighted'] = 'flag for evaluating the fit objective on log(I)'
         self.input_doc['q_range'] = 'lower and upper q-limits for the fit objective'
-        self.input_doc['good_fit_prior'] = 'initial setting for the good fit indicator'
         self.output_doc['system'] = 'xrsdkit.system.System with optimized parameters'
         self.output_doc['system_dict'] = 'output `system`.to_dict()'
         self.output_doc['q_I_opt'] = 'computed intensity for the optimized system'
-        self.output_doc['good_fit_flag'] = 'flag indicating user satisfaction with the fit'
 
     def run(self):
         q_I = self.inputs['q_I']
@@ -44,15 +40,14 @@ class XRSDFitGUI(Operation):
         err_wtd = self.inputs['error_weighted']
         logI_wtd = self.inputs['logI_weighted']
         q_range = self.inputs['q_range']
-        good_fit_prior = self.inputs['good_fit_prior']
         if sys is not None:
             if sys.fit_report:
                 err_wtd = sys.fit_report['error_weighted']
                 logI_wtd = sys.fit_report['logI_weighted']
                 q_range = sys.fit_report['q_range']
 
-        sys_opt, good_fit_flag = run_fit_gui(
-        sys,q_I[:,0],q_I[:,1],src_wl,None,err_wtd,logI_wtd,q_range,good_fit_prior) 
+        sys_opt = run_fit_gui(
+        sys,q_I[:,0],q_I[:,1],src_wl,None,err_wtd,logI_wtd,q_range) 
 
         I_opt = sys_opt.compute_intensity(q_I[:,0],src_wl) 
         q_I_opt = np.array([q_I[:,0],I_opt]).T
@@ -60,6 +55,5 @@ class XRSDFitGUI(Operation):
         self.outputs['system'] = sys_opt
         self.outputs['system_dict'] = sys_opt.to_dict()
         self.outputs['q_I_opt'] = q_I_opt
-        self.outputs['good_fit_flag'] = good_fit_flag 
         return self.outputs
 
