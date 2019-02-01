57a027eb98cdd035ff522367733791ccc23beba9
Lenson <lenson@slac.stanford.edu>
2019-02-01 11:35:09 -0800

    several small cleanups

--- paws/workflows/IO/BL15/ReadTimeSeries.py
+++ paws/workflows/IO/BL15/ReadTimeSeries.py
@@ -20,20 +20,20 @@ class ReadTimeSeries(Workflow):
     def __init__(self):
         super(ReadTimeSeries,self).__init__(inputs,outputs)
         self.add_operation('read_batch',ReadBatch.ReadBatch())
-        self.add_operation('sort',SortBatch())
 
     def run(self):
         read_inputs = OrderedDict([(k,self.inputs[k]) for k in ReadBatch.inputs.keys()])
         b_outs = self.operations['read_batch'].run_with(**read_inputs) 
+        sort = SortBatch()
         self.operations['sort'].run_with(
             batch_outputs=b_outs,
-            x_key='time',
+            x_values=b_outs['time'],
             x_sort_flag=True,
             x_shift_flag=True,
             lower_index=self.inputs['lower_index'],
             upper_index=self.inputs['upper_index'],
             index_step=self.inputs['index_step']
             )
-        self.outputs.update(self.operations['sort'].outputs['sorted_outputs'])
+        self.outputs.update(sort.outputs['sorted_outputs'])
         return self.outputs
 
