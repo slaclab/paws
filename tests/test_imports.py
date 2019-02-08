9fd4ee9c256927c34b98a1226067e67720d62d87
Lenson Pellouchoud <lensonp@gmail.com>
2019-02-07 17:08:13 -0800

    removed old wf tests

---
+++ tests/test_imports.py
@@ -0,0 +1,15 @@
+import paws
+from paws import operations
+from paws import workflows
+from paws import plugins
+
+from paws.operations import PROCESSING, IO, DAQ, PACKAGING, TESTS, VISUALIZATION
+
+from paws.workflows import IO, BACKGROUND, DAQ, FITTING, INTEGRATION, VISUALIZATION
+
+from paws.plugins import Timer, SSHClient, MitosPPumpController, FlowReactor, \
+    BayesianFlowDesigner, CryoConController, PyFAIIntegrator, SpecInfoClient, MarCCDClient
+
+def test_imports():
+    return True
+
