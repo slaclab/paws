ea67eb50ff13173a308ca2da15d03414f9d5dbff
Lenson Pellouchoud <lensonp@gmail.com>
2019-02-22 18:04:49 -0800

    updated test

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -5,7 +5,7 @@ from paws import plugins
 
 from paws.operations import PROCESSING, IO, DAQ, PACKAGING, TESTS
 
-from paws.workflows import IO, DAQ, FITTING, INTEGRATION, VISUALIZATION
+from paws.workflows import IO, DAQ
 
 from paws.plugins import Timer, SSHClient, MitosPPumpController, FlowReactor, \
     BayesianDesigner, CitrinationDesigner, CryoConController, PyFAIIntegrator, SpecInfoClient, MarCCDClient
