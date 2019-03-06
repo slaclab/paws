6acf3b53eb584b159147a9f1f69d99936040b2fa
Lenson Pellouchoud <lensonp@gmail.com>
2019-03-05 16:50:03 -0800

    corrected workflows for removal of deprecated ops

--- paws/workflows/IO/BL15/Read.py
+++ paws/workflows/IO/BL15/Read.py
@@ -5,9 +5,6 @@ import os
 from xrsdkit.tools import ymltools as xrsdyml
 
 from ...Workflow import Workflow 
-from ....operations.IO.YAML.LoadYAML import LoadYAML
-from ....operations.IO.IMAGE.FabIOOpen import FabIOOpen
-from ....operations.IO.NumpyLoad import NumpyLoad
 
 # NOTE: this workflow is for reading samples
 # that were saved with YAML headers
@@ -32,16 +29,16 @@ class Read(Workflow):
 
     def __init__(self):
         super(Read,self).__init__(inputs,outputs)
-        self.header_reader = LoadYAML()
         self.image_reader = FabIOOpen()
-        self.q_I_reader = NumpyLoad()
+
+    def read_header(self,filepath):
+        return yaml.load(open(filepath,'r'))
 
     def run(self):
         self.outputs = copy.deepcopy(outputs)
 
         if (self.inputs['header_file']) and (os.path.exists(self.inputs['header_file'])):
-            hdr_outs = self.header_reader.run_with(
-            file_path = self.inputs['header_file'])
+            hdata = self.read_header(self.inputs['header_file'])
             hdata = hdr_outs['data']
             self.outputs['header_data'] = hdata
             self.outputs['time'] = hdata['time']
@@ -49,16 +46,12 @@ class Read(Workflow):
             self.message_callback('header file not found: {}'.format(self.inputs['header_file']))
 
         if (self.inputs['image_file']) and (os.path.exists(self.inputs['image_file'])):
-            img_outs = self.image_reader.run_with(
-            file_path=self.inputs['image_file'])
-            self.outputs['image_data'] = img_outs['image_data']
+            self.outputs['image_data'] = fabio.open(self.inputs['image_file'])
         elif self.inputs['image_file']:
             self.message_callback('image file not found: {}'.format(self.inputs['image_file']))
 
         if (self.inputs['q_I_file']) and (os.path.exists(self.inputs['q_I_file'])):
-            q_I_outs = self.q_I_reader.run_with(
-            file_path=self.inputs['q_I_file'])
-            q_I = q_I_outs['data']
+            q_I = np.loadtxt(self.inputs['q_I_file'],dtype='float') 
             dI = None
             if (q_I is not None) and (q_I.shape[1] > 2):
                 q_I = q_I[:,:2]
