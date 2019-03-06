1e1d3fb25819b7886c43db21d3b53cfd5a21eafd
Lenson Pellouchoud <lensonp@gmail.com>
2019-03-05 16:50:13 -0800

    new imports test

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -3,9 +3,19 @@ from paws import operations
 from paws import workflows
 from paws import plugins
 
-from paws.operations import PROCESSING, IO, DAQ, PACKAGING, TESTS
+from paws.operations import ARRAYS, BACKGROUND, CALIBRATION, FILESYSTEM, \
+    SMOOTHING, SORTING, SSRL_BEAMLINE_1_5, TESTS, ZINGERS 
+from paws.operations.ARRAYS import ArrayYMean, NoiseArray
+from paws.operations.BACKGROUND import BgSubtract
+from paws.operations.CALIBRATION import ReadPONI, Fit2DToPONI, NikaToPONI, WXDToPONI 
+from paws.operations.FILESYSTEM import BuildFileList 
+from paws.operations.SMOOTHING import MovingAverage, SavitzkyGolay 
+from paws.operations.SORTING import SortBatch 
+from paws.operations.SSRL_BEAMLINE_1_5 import ReadSpecHeader
+from paws.operations.TESTS import Print, ListPrimes
+from paws.operations.ZINGERS import EasyZingers1d 
 
-from paws.workflows import IO, DAQ
+#from paws.workflows import IO, DAQ
 
 from paws.plugins import Timer, SSHClient, MitosPPumpController, FlowReactor, \
     BayesianDesigner, CitrinationDesigner, CryoConController, PyFAIIntegrator, SpecInfoClient, MarCCDClient
